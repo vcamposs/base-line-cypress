eae2f9f1bf19850093a8c6f7bd922559b12cebb0
Victor Campos Silva <vcampos@lnxcit019496.cit>
2019-06-24 15:00:24 -0300

    alter

--- cypress/page_objects/base.page.js
+++ cypress/page_objects/base.page.js
@@ -1,30 +1,44 @@
 const basePage = {
     openUrl: page => {
-		cy.visit(page);
+        cy.visit(page);
     },
     verifyUrl: url => {
         cy.url().should('include', url);
     },
     clickButton: button => {
-        cy.get(pageObjects.getDataTest(button))
-		.first()
-		.click();
+        // cy.get(pageObjects.getDataTest(button))
+        // .first()
+        // .click();
+        // cy.get(pageObjects.getDataTest(button)).then(teste => {
+        //     teste.array.forEach(element => {
+                
+        //     });
+        //     teste[1].click();
+        // });
+        // cy.get(pageObjects.getDataTest(button)).each(()) => {
+
+        // })
+
+        cy.get(pageObjects.getDataTest(button)).each((buttons, i) => {
+            console.log(i);
+            console.log(buttons[i].click());
+          })
     },
     fillField: (field, text) => {
         cy.get(pageObjects.getDataTest(field))
-		.first()
-		.focus()
-		.type(text);
+            .first()
+            .focus()
+            .type(text);
     },
-    verifyLabel: (element,label) => {
+    verifyLabel: (element, label) => {
         cy.get(pageObjects.getDataTest(element)).should('have.contain', label);
     }
 };
 
 const pageObjects = {
-	getDataTest(id) {
-		return `[data-test="${id}"]`;
-	}
+    getDataTest(id) {
+        return `[data-test="${id}"]`;
+    }
 };
 
 module.exports = basePage;
